e0232b5af1700c7f93de446ce23470d7041b97ac
Shaswat Paudel <shaswat@ezops.ca>
2023-03-16 13:35:58 -0600

    auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common";
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+  signin() {
+    return { msg: 'this is sign in' }
+  }
+
+  signup() {
+    return { msg: 'this is sign in' }
+  }
+}
