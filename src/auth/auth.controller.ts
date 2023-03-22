c086d7216ef6c13dcd06a50660bca3403ab7fab8
Shaswat Paudel <shaswat@ezops.ca>
2023-03-22 15:21:29 -0600

    dto added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,22 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
 @Controller('auth')
 export class AuthController {
-  constructor (private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
+
+  @Post('signup')
+  signup(
+    @Body('email') email: string,
+    @Body('password') password: string
+  ) {
+    return this.authService.signup()
+  }
 
   @Post('signin')
   signin() {
     return this.authService.signin()
   }
 
-  @Post('signup')
-  signup(){
-    return this.authService.signup()
-  }
+
 }
---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail } from "class-validator"
+import { IsNotEmpty, IsString } from "class-validator/types/decorator/decorators"
+
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string
+
+  @IsString()
+  @IsNotEmpty()
+  password: string
+}
